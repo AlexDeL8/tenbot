fe58af3cbe8e812e343d86c728f892ee35c9d15f
Alexander De Luca <alexnaj88@gmail.com>
2020-08-15 17:53:23 -0400

    added tmi js and base config file for client

---
+++ conf/config.js
@@ -0,0 +1,17 @@
+const config = {
+    bot_username: "TENBot_sl",
+    //bot_username: "ENTER_BOT_USERNAME_HERE",
+    bot_pass_oauth: "oauth:gl935b5w8qkweuzrszj1v8kmx57uyt",
+    //bot_pass_oauth: "ENTER_BOT_OAUTH_TOKEN_HERE",
+    channels: ["#theelitenax"],
+    //channels: ["#ENTER_YOUR_TWITCH_USERNAME_HERE_lowercase"],
+    gamertag: "The Elite Nax",
+    //gamertag: "ENTER_GAMERTAG_HERE",
+    secureConnection: true, //recommended
+    reconnect: true, //recommended
+    debug: true //recommended
+}
+
+module.exports = {
+    config: config
+};
