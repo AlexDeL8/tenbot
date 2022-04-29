b14d8ea001d5874fb70468d9ef984243cdab6004
Alex De Luca <alexnaj88@gmail.com>
2022-04-28 23:43:38 -0400

    added currentGame call with logic, API call, and bot response message

--- services/xbox_achievement_service.js
+++ services/xbox_achievement_service.js
@@ -1,48 +1,94 @@
-'use strict';
+"use strict";
 
-import { xboxApiConfig } from '../config/config.js'
-import fetch from 'node-fetch';
+import { xboxApiConfig } from "../config/config.js";
+import fetch from "node-fetch";
 
 class XboxAchievementService {
     constructor() {
-        this.gamerscore = '';
+        this.gamerscore = "";
+        this.currentGameStats = "";
+
+        this.cache = {
+            gamerscore: "",
+            games: {
+                total: "",
+                XOneGames: "",
+                X360Games: "",
+            },
+        };
     }
+
+    //Private
+    _getCache(propertyName = "") {}
+
+    //Private
+    _updateCache(propertyName = "") {}
 
     async getGamerscore() {
-        if(this.gamerscore !== '') {
-            return this.gamerscore;
-        } else {
-            let profileInfoForXuidValue = await fetch(`${xboxApiConfig.basePath}${xboxApiConfig.xuid}/new-profile`, {
-                method: 'GET',
+        // if (this.gamerscore !== "") {
+        //     return this.gamerscore;
+        // } else {
+        let profileInfoForXuidValue = await fetch(
+            `${xboxApiConfig.basePath}${xboxApiConfig.xuid}/new-profile`,
+            {
+                method: "GET",
                 headers: {
-                    'X-AUTH': xboxApiConfig.apiKey,
-                    'Content-Type': 'application/json'
-                }
-            });
-            
-            let profileInfoForXuidJson = await profileInfoForXuidValue.json();
-            this.gamerscore = this._formatNumber(profileInfoForXuidJson.gamerScore);
-            return this.gamerscore;
-        }
+                    "X-AUTH": xboxApiConfig.apiKey,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
+        let profileInfoForXuidJson = await profileInfoForXuidValue.json();
+        this.gamerscore = this._formatNumber(profileInfoForXuidJson.gamerScore);
+        return this.gamerscore;
+        // }
     }
 
-    getTotalGames() {
-        return '';
+    async getCurrentGame() {
+        let profileCurrentGameStatsValue = await fetch(
+            `${xboxApiConfig.basePath}${xboxApiConfig.xuid}/titlehub-achievement-list`,
+            {
+                method: "GET",
+                headers: {
+                    "X-AUTH": xboxApiConfig.apiKey,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
+        let profileCurrentGameStatsJson =
+            await profileCurrentGameStatsValue.json();
+
+        //Take first item, most recent TODO: Find way to trim call itself configuring fetch()?
+        //NOTE: Possible expensive call to track
+        let currentGameAchievementStats =
+            profileCurrentGameStatsJson.titles[0].achievement;
+
+        this.currentGameStats = {
+            title: profileCurrentGameStatsJson.titles[0].name,
+            achievements: currentGameAchievementStats.currentAchievements,
+            gamerscore: this._formatNumber(
+                currentGameAchievementStats.currentGamerscore
+            ),
+            completionPercent: currentGameAchievementStats.progressPercentage,
+        };
+        return this.currentGameStats;
     }
 
     //Private
     _formatNumber(numberToFormat) {
-        return numberToFormat.toLocaleString('en-US');
+        return numberToFormat.toLocaleString("en-US");
     }
 
     //Private
     _getXOneGames() {
-        return '';
+        return "";
     }
 
     //Private
     _get360Games() {
-        return '';
+        return "";
     }
 }
 
--- subscribers/command_handler.js
+++ subscribers/command_handler.js
@@ -1,27 +1,40 @@
-'use strict'
+"use strict";
 
-import { tenBot } from '../tenbot.js';
-import { commonEmitter } from '../common_emitter.js';
-import { XboxAchievementService } from '../services/xbox_achievement_service.js';
+import { tenBot } from "../tenbot.js";
+import { commonEmitter } from "../common_emitter.js";
+import { XboxAchievementService } from "../services/xbox_achievement_service.js";
 
 const xboxAchievementService = new XboxAchievementService();
 
 //Subscribe to '!command'
-commonEmitter.on('commandSent', function(commandString, replyMessage, channel, tags) {
-	switch(commandString) {
-		case 'gamerscore':
-			xboxAchievementService.getGamerscore().
-			then( returnedGamerscore => {
-				replyMessage = `@${tags['display-name']} , Nax\'s current Gamerscore: ${returnedGamerscore}G`;
-				tenBot.say(channel, replyMessage);
-			}).
-			catch(err => console.log(`ERROR - !gamerscore .on(): ${err}`));
-			
-			break;
-		case 'totalGames':
-			let totalGames = 8;
-			replyMessage = `@${tags['display-name']} , Nax\'s total games: ${totalGames}`;
-			tenBot.say(channel, replyMessage);
-			break
-	}
-});
+commonEmitter.on(
+    "commandSent",
+    function (commandString, replyMessage, channel, tags) {
+        switch (commandString) {
+            case "gamerscore":
+                xboxAchievementService
+                    .getGamerscore()
+                    .then((returnedGamerscore) => {
+                        replyMessage = `@${tags["display-name"]} , Nax\'s current Gamerscore: ${returnedGamerscore}G`;
+                        tenBot.say(channel, replyMessage);
+                    })
+                    .catch((err) =>
+                        console.log(`ERROR - !gamerscore : ${err}`)
+                    );
+
+                break;
+            case "currentGame":
+                xboxAchievementService
+                    .getCurrentGame()
+                    .then((returnedCurrentGame) => {
+                        replyMessage = `@${tags["display-name"]} , ${returnedCurrentGame.title} current stats: ${returnedCurrentGame.gamerscore}G -- ${returnedCurrentGame.achievements} achievements won -- ${returnedCurrentGame.completionPercent}% complete! PogChamp`;
+                        tenBot.say(channel, replyMessage);
+                    })
+                    .catch((err) =>
+                        console.log(`ERROR - !currentGame : ${err}`)
+                    );
+
+                break;
+        }
+    }
+);
